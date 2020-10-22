050f1d338bc1ad2b1868e74fd912af138ef85a1a
Jamilly Pinheiro Lima <jamillyp@alu.ufc.br>
2020-10-21 23:36:19 -0300

    update; back-end

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express';
 import './controllers/OrphanagesController';
+
+import multer from 'multer';
+import uploadConfig from './config/upload';
 import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
-// rota para criar um orfanato
-routes.post('/orphanages', OrphanagesController.create)
+// rota para criar um orfanato e adcionar imagens a esse orfanato
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create)
 export default routes;
 
 // rota para listar orfanatos
 routes.get('/orphanages', OrphanagesController.index)
+
+// rota para listar um único orfanato por seu id
+routes.get('/orphanages/:id', OrphanagesController.show)
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,17 +1,20 @@
 import express from 'express';
 import './database/connection';
+import 'express-async-errors';
+import cors from 'cors';
+
+import path from 'path';
 
 import routes from './routes';
-
+import errorHandler from './errors/handler';
 
 const app = express(); // chamando o express pra atuar 
 
+app.use(cors());
 app.use(express.json()); //para o express ler codigos json
 app.use(routes); //para executar as rotas
-
-//console.log(request.query); // mostra uma busca especifica
-//console.log(request.params); // mostra o id
-//console.log(request.body); // mostra tudo da requisicao
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads'))) 
+app.use(errorHandler);
 
 // porta do servidor que irá executar o back-end
 app.listen(3333);
